2d3765d900d126b1132432c77a0d3b9458c81322
ArikMackenburg <Arikmackenburg@gmail.com>
2020-10-11 02:40:39 -0500

    Some of the cart and other stuff working

---
+++ Web/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Models.Products;
+
+namespace Web.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Web.Data.ApplicationDbContext _context;
+
+        public IndexModel(Web.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
+        }
+    }
+}
